b112b26f23ea5b295100a3b3c76306a894c85682
Christina <christina.smithers@gmail.com>
2018-12-07 16:08:53 -0800

    Grand total row added as well as navbar

--- index.php
+++ index.php
@@ -82,7 +82,54 @@ define("SCRIPT_ROOT", "http://localhost:8000/tools/");
             padding: 0 0 0 10px;
         }
     </style>
-
+  <div class="top-bar">
+    <div class="top-bar-title">
+      <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
+        <button class="menu-icon dark" type="button" data-toggle></button>
+      </span>
+      <img id="barLogo" src="https://tapstone.com/tools/images/tapstoneLogo.png">
+    </div>
+    <div id="responsive-menu">
+      <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+          <li>
+            <a href="https://tapstone.com/tools/">Tools</a>
+            <ul class="menu vertical">
+              <li>
+                <a href="https://tapstone.com/tools/DoubleDateDashboard/">Double Date Dashboard</a>
+              </li>
+              <li>
+                <a href="https://tapstone.com/tools/AffSub/">Aff Sub Report</a>
+              </li>
+              <li>
+                <a href="https://tapstone.com/tools/FeedReport/">Feed Report</a>
+              </li>
+              <li>
+                <a href="https://tapstone.com/tools/NetsphereReport/">Netsphere Report</a>
+              </li>
+              <li>
+                <a href="https://tapstone.com/tools/PayoutRevEdit/">Payout & Revenue Editor</a>
+              </li>
+              <li>
+                <a href="https://tapstone.com/tools/1QIS/">1QIS Daily Report</a>
+              </li>
+              <li>
+                <a href="https://tapstone.com/tools/PublisherBidManager/">Publisher Bid Manager</a>
+              </li>
+              <li>
+                <a href="https://tapstone.com/tools/HONeY/">Yahoo Tool</a>
+              </li>
+              <li>
+                <a href="https://tapstone.com/tools/TaboolaTruePMReloaded/">Taboola TruePMv2</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="top-bar-right">
+      </div>
+    </div>
+  </div>
     <br />
 
     <div class="row expanded">
